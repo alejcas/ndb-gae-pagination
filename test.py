a5674d353577dd7ad9915d2b0d812b04c5434c8d
Alejandro <janscas@users.noreply.github.com>
2014-04-30 11:29:09 +0200

    Update test.py

--- test.py
+++ test.py
@@ -8,12 +8,31 @@ class Note(ndb.Model):
   
 
 def test:
-    for i in range(3):
-        n = Note("test true", True)
-        n.put()
-    for i in range(3):
-        n = Note("test false", False)
-        n.put()
+    n = []
+    n.append(Note("test1", True))
+    n.append(Note("test2", True))
+    n.append(Note("test3", False))
+    n.append(Note("test4", True))
+    n.append(Note("test5", True))
+
+    n.put_multi(n)
     
+    # usually this function is included as a class method inside a Model so cls is always the class calling
+    results, prev_b, next_b = return_query_page(Note, 1, None, None, {'published': True}, {'creation_date': '-'})
+    assert results[0].text == "test1'
     
+    #calling next page
+    results, prev_b, next_b = return_query_page(Note, 1, next_b, None, {'published': True}, {'creation_date': '-'})
+    assert results[0].text == "test2'
     
+    #calling next page
+    results, prev_b, next_b = return_query_page(Note, 1, next_b, None, {'published': True}, {'creation_date': '-'})
+    assert results[0].text == "test4'
+    
+    #calling prev page
+    results, prev_b, next_b = return_query_page(Note, 1, prev_b, True, {'published': True}, {'creation_date': '-'})
+    assert results[0].text == "test2'
+    
+    #calling prev page
+    results, prev_b, next_b = return_query_page(Note, 1, prev_b, True, {'published': True}, {'creation_date': '-'})
+    assert results[0].text == "test1'
